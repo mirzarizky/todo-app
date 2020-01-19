ed6c0e6db1c24576f968ee7c917687f62c0239ba
Mirza Rizky <mirzarizky@gmail.com>
2020-01-19 15:54:09 +0700

    timestamp fix

--- index.php
+++ index.php
@@ -28,10 +28,10 @@
                 $name = $_POST['name'];
                 $email = $_POST['email'];
                 $job = $_POST['job'];
-                $date = date("Y-m-d");
+
                 // Insert data
                 $sql_insert = "INSERT INTO users (name, email, job, created_at) 
-                            VALUES (?,?,?,?)";
+                            VALUES (?,?,?,(current_timestamp))";
                 $stmt = $conn->prepare($sql_insert);
                 $stmt->bindValue(1, $name);
                 $stmt->bindValue(2, $email);
